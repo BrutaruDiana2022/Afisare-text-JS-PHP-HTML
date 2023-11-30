4affb455196b0820957302110cd2df3dd58af2ba
BrutaruDiana2022 <119977400+BrutaruDiana2022@users.noreply.github.com>
2023-11-30 17:11:15 +0200

    Add files via upload
    Mesaje scrise afisate cu JavaScript/PHP/HTML

---
+++ js2.php
@@ -0,0 +1,12 @@
+<html>
+    <head>        
+    </head>
+    <body>  
+        <h3>Afisarea textului cu JavaScript/PHP/HTML</h3> <hr/>            
+        <script type="text/javascript">            
+            document.write("Acest mesaj e scris cu JavaScript");            
+        </script> <br>
+        <?php echo "Acest mesaj e scris cu PHP";?> <br>
+        Acest mesaj e scris cu HTML
+    </body>
+</html>
